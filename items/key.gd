b509ceb1d830037c48bcae45a7d8c1d59ad2bf29
Niels Hansen <nielsh@gmail.com>
2019-06-22 21:07:37 -0600

    updated scenes

--- items/key.gd
+++ items/key.gd
@@ -1,25 +1,37 @@
 extends Area2D
 
+onready var camera = get_node('../camera')
+onready var effect = get_node('effect')
+onready var sprite = get_node('Sprite')
+
+var maxDistance = 100.0
+var rotateSpin = 2.5
+var isFound = false
+
 func _ready():
 	connect("body_entered", self, "bodyEntered")
 	
+	effect.interpolate_property(sprite, 'scale', sprite.transform.get_scale(),
+		Vector2(0.0, 0.0), 0.4, 
+		Tween.TRANS_QUAD, Tween.EASE_OUT)
+		
+	effect.interpolate_property(sprite, 'modulate', Color(1, 1, 1, 1), 
+		Color(1, 1, 1, 0), 0.4,
+		Tween.TRANS_QUAD, Tween.EASE_OUT)
+	
 func _process(delta):
-	var camera = get_node('../camera')
-	
+		
 	var pos = global_position - Vector2(0,0)
-	var x = floor(pos.x / 448) * 448
-	var y = floor(pos.y / 256) * 256
-	
-	var roomX = floor(x / 448)
-	var roomY = floor(y / 256)
+	var roomX = floor(pos.x / 448)
+	var roomY = floor(pos.y / 256)
 	
 	if camera.roomX == roomX && camera.roomY == roomY:
 		var playerNode = get_node('../player')
 		var distanceToPlayer = playerNode.global_position.distance_to(global_position); 
-		if (distanceToPlayer < 100.0):
-			var ang = get_angle_to(playerNode.position)
-			rotate(ang* 1.5 * delta)
-			var speed = (100.0 - distanceToPlayer) / 2.0
+		
+		if (distanceToPlayer < maxDistance):
+			rotate(get_angle_to(playerNode.position) * rotateSpin * delta)
+			var speed = (maxDistance - distanceToPlayer) / 2.0
 			var direction = (playerNode.position - position).normalized()
 			var motion = direction * speed * delta
 			position += motion
@@ -27,6 +39,11 @@ func _process(delta):
 func bodyEntered(body):
 	
 	if (body.name == 'player'):
+		isFound = true
 		body.keys += 1
-		queue_free()
+		effect.start()
 		
+		
+
+func _on_effect_tween_completed(object, key):
+	queue_free()
